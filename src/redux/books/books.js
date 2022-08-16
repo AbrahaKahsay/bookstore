2df509bf24212879d81334fdbff9725c6eb54672
Abraha Kahsay <abrahak81@gmail.com>
2022-08-16 18:16:47 +0300

    Organize codes to avoid errors

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -27,16 +27,13 @@ export const removeBook =(id)=>{
 const booksReducer = (state=initialState, action)=>{
 switch(action.type){
     case ADD_BOOK:
-        return
-            [...state, {
+        return [...state, {
                 title:action.title,
                 author: action.author,
                 id: action.id
-            }
-        ]
+            }]
     case REMOVE_BOOK:
-        return 
-        state.filter(book => book.id !== action.id)
+        return state.filter(book => book.id !== action.id)
     default: 
     return state
 
