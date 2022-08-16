6ae48fd62f2b0582191a769f0477efcfa51f3574
Abraha Kahsay <abrahak81@gmail.com>
2022-08-16 18:36:16 +0300

    combine reducers using configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./books/books";
 import statusReducer from "./categories/categories";
+
+const store = configureStore({
+    reducer: {
+        booksReducer,
+        statusReducer,
+    }
+})
