71b1b439a1ded238e71e6cd9fdcf8932d9b8f0de
Abraha Kahsay <abrahak81@gmail.com>
2022-08-17 19:01:00 +0300

    displays books from Redux store using useSelector hook

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
-  const [books] = useState([
-    {
-      title: 'The Alchemist',
-      author: 'Paulo Cohelo',
-      id: Math.random(),
-    }]);
-
+  // const [books] = useState([
+  //   {
+  //     title: 'The Alchemist',
+  //     author: 'Paulo Cohelo',
+  //     id: Math.random(),
+  //   }]);
+  const books = useSelector((state) => state.booksReducer);
   return (
     <div className="books">
       <div>
