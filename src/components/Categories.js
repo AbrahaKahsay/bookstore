63bf39facdf7e4f199089af45b273d51c18c98e3
Abraha Kahsay <abrahak81@gmail.com>
2022-08-18 16:37:26 +0300

    Add status functionality in categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
+  const state = useSelector((state) => state.statusReducer);
   const dispatch = useDispatch();
   return (
-    <>
+    <div>
+      <p>{state}</p>
       <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
-    </>
+    </div>
   );
 };
 export default Categories;
