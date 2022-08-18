6b893dc6e3b4aabe9469319a00fc22a38730c9a8
Abraha Kahsay <abrahak81@gmail.com>
2022-08-18 16:12:15 +0300

    Add book id prop to Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,4 +1,4 @@
-// import React, { useState } from 'react';
+/* eslint-disable max-len */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Book from './Book';
@@ -9,7 +9,7 @@ const Books = () => {
   return (
     <div className="books">
       <div>
-        {books.map((book) => (<Book title={book.title} author={book.author} key={book.id} />))}
+        {books.map((book) => (<Book title={book.title} id={book.id} author={book.author} key={book.id} />))}
       </div>
       <AddBookForm />
     </div>
