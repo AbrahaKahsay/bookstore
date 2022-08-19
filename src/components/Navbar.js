4276f154728e94e56b9c6f643142d64ed6762d7e
Abraha Kahsay <abrahak81@gmail.com>
2022-08-20 02:40:13 +0300

    Add style to Nab bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
+import './Navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav>
+  <nav className="nav">
     <header className="books-header">
       <h1> Bookstore CMS</h1>
     </header>
-    <ul>
-      <li><Link to="/">Books</Link></li>
-      <li><Link to="/categories">Categories</Link></li>
+    <ul className="links-wraper">
+      <li><Link to="/" className="Books">BOOKS</Link></li>
+      <li><Link to="/categories" className="Categories">CATEGORIES</Link></li>
     </ul>
   </nav>
 );
